1e55afc3781eb634759407e7c40f849851b4e4b5
Dick Kipngeno Korir <DKORIR2@safaricom.co.ke>
2022-05-05 19:10:53 +0300

    Initial commit

---
+++ src/main/java/com/java/reactive/controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.java.reactive.controller;
+
+import com.java.reactive.dto.ProductDto;
+import com.java.reactive.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public Flux<ProductDto> getProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ProductDto> getProduct(@PathVariable String id) {
+        return service.getProduct(id);
+    }
+
+    @GetMapping("/product-range")
+    public Flux<ProductDto> getProductBetweenRange(
+            @RequestParam("max") double max,
+            @RequestParam("min") double min) {
+        return service.getProductInRange(min, max);
+    }
+
+    @PostMapping
+    public Mono<ProductDto> saveProduct(@RequestBody Mono<ProductDto> productDtoMono) {
+        return service.saveProduct(productDtoMono);
+    }
+
+    @PutMapping("/update/{id}")
+    public Mono<ProductDto> updateProduct(@RequestBody Mono<ProductDto> productDtoMono, @PathVariable String id) {
+        return service.updateProduct(productDtoMono, id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Mono<Void> deleteProduct(@PathVariable String id) {
+        return service.deleteProduct(id);
+    }
+}
---
+++ src/main/java/com/java/reactive/utils/AppUtils.java
@@ -0,0 +1,22 @@
+package com.java.reactive.utils;
+
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.java.reactive.dto.ProductDto;
+import com.java.reactive.entity.Product;
+import org.springframework.beans.BeanUtils;
+
+public class AppUtils {
+
+
+    public static ProductDto entityToDto(Product product) {
+        ProductDto productDto = new ProductDto();
+        BeanUtils.copyProperties(product, productDto);
+        return productDto;
+    }
+
+    public static Product dtoToEntity(ProductDto productDto) {
+        Product product = new Product();
+        BeanUtils.copyProperties(productDto, product);
+        return product;
+    }
+}
